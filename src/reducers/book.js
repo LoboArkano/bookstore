eca88f853ac9b54df0bf9a3ef52ee006c860af93
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-11 12:58:54 -0600

    Implement book reducer

--- src/reducers/book.js
+++ src/reducers/book.js
@@ -1,4 +1,24 @@
-// eslint-disable-next-line no-unused-vars
-const bookReducer = (state = null, action) => state;
+const ADD_BOOK = 'ADD_BOOK';
+const REMOVE_BOOK = 'REMOVE_BOOK';
+const INITIAL_STATE = {
+  title: 'Unkown',
+  category: 'Action',
+};
+
+const bookReducer = (state = INITIAL_STATE, action) => {
+  const { book } = action.payload;
+
+  switch (action.types) {
+    case ADD_BOOK:
+      return {
+        ...state,
+        book,
+      };
+    case REMOVE_BOOK:
+      return state.select(bookState => bookState.id === book.id);
+    default:
+      return state;
+  }
+};
 
 export default bookReducer;
