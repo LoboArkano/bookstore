e30afebdbb19789b067cba1967142f072fcf4f68
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-21 15:00:02 -0600

    Change to stateful component with hooks

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
 
-const BooksForm = () => (
-  <div>
-    <form>
-      <label htmlFor="title">
-        Title:
-        <input type="text" id="title" name="title" placeholder="Book Title" />
-      </label>
-      <select>
-        {
-          bookCategories.map(category => (
-            <option key={category} value={category}>{ category }</option>
-          ))
-        }
-      </select>
-      <button type="submit">ADD</button>
-    </form>
-  </div>
-);
+const BooksForm = () => {
+  const [state, setState] = useState({
+    title: '',
+    category: '',
+  });
+
+  return (
+    <div>
+      <form>
+        <label htmlFor="title">
+          Title:
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Book Title"
+            value={state.title}
+            required
+          />
+        </label>
+        <select
+          name="category"
+          value={state.category}
+          required
+        >
+          <option key="none" value="none">None</option>
+          {
+            bookCategories.map(category => (
+              <option
+                key={category}
+                value={category}
+              >
+                { category }
+              </option>
+            ))
+          }
+        </select>
+        <button type="submit">ADD</button>
+      </form>
+    </div>
+  );
+};
 
 export default BooksForm;
