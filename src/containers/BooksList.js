114fb17305a75710b44fc79f74e9f7deeb4b0194
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-22 14:38:15 -0600

    Add handleFilterChange Add filteredBooks function Render CategoryFilter

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { removeBook } from '../actions/index';
+import { removeBook, changeFilter } from '../actions/index';
 import Book from '../components/Book';
+import CategoryFilter from '../components/CategoryFilter';
 
 const BooksList = props => {
-  const { books, removeBook } = props;
+  const {
+    books, removeBook,
+    filter, changeFilter,
+  } = props;
+
+  const filteredBooks = (books, filter) => {
+    if (filter === 'All') {
+      return books;
+    }
+
+    return books.filter(book => book.category === filter);
+  };
 
   const handleRemoveBook = book => {
     removeBook(book);
+  };
+
+  const handleFilterChange = e => {
+    changeFilter(e.target.value);
   };
 
   const getContent = books => books.map(book => (
@@ -16,19 +32,22 @@ const BooksList = props => {
   ));
 
   return (
-    <table key="booksList">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Title</th>
-          <th>Category</th>
-          <th>Remove</th>
-        </tr>
-      </thead>
-      <tbody>
-        {getContent(books)}
-      </tbody>
-    </table>
+    <>
+      <table key="booksList">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Remove</th>
+          </tr>
+        </thead>
+        <tbody>
+          {getContent(filteredBooks(books, filter))}
+        </tbody>
+      </table>
+      <CategoryFilter changeFilter={handleFilterChange} />
+    </>
   );
 };
 
@@ -39,12 +58,16 @@ BooksList.propTypes = {
     category: PropTypes.string.isRequired,
   })).isRequired,
   removeBook: PropTypes.func.isRequired,
+  changeFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => {
-  const { books } = state;
+  const { books, filter } = state;
 
-  return { books };
+  return { books, filter };
 };
 
-export default connect(mapStateToProps, { removeBook })(BooksList);
+const mapDispatchToProps = { removeBook, changeFilter };
+
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
