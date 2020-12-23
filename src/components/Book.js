e89e0dbae1b90eb4dd21fc9ad6fdadf79264652b
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-23 17:57:50 -0600

    Add style for the book info

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../assets/stylesheets/book.css';
 
 const Book = props => {
   const { book, removeBook } = props;
   const { id, title, category } = book;
 
   return (
-    <tr>
-      <td>{id}</td>
-      <td>{title}</td>
-      <td>{category}</td>
-      <td>
-        <button type="button" onClick={() => removeBook(id)}>Remove Book</button>
-      </td>
-    </tr>
+    <article className="book-container d-flex">
+      <div className="left d-flex justify-c-sb h-100">
+        <div className="d-flex f-dir-c">
+          <h3 className="category">{category}</h3>
+          <h2 className="title">{title}</h2>
+          <h4 className="author">Author</h4>
+          <ul className="list d-flex">
+            <li className="item">
+              <button className="action" type="button">Comments</button>
+            </li>
+            <li className="item">
+              <button
+                className="action"
+                type="button"
+                onClick={() => removeBook(id)}
+              >
+                Remove
+              </button>
+            </li>
+            <li className="item">
+              <button className="action" type="button">Edit</button>
+            </li>
+          </ul>
+        </div>
+        <div>
+          Numero
+        </div>
+      </div>
+      <div className="right">
+        right
+      </div>
+    </article>
   );
 };
 
