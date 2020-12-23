b67e8a54f57abaf37161e296d46627c126e97d5b
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-23 11:52:06 -0600

    Add header with style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import avatar from '../assets/images/man.png';
+import '../assets/stylesheets/header.css';
 
 const Header = () => (
-  <header>
-    <div>
-      <h1>Bookstore CMS</h1>
-      <ul>
-        <li>BOOKS</li>
-        <li>CATEGORIES</li>
-      </ul>
-    </div>
-    <div>
-      <img src={avatar} alt="avatar" />
-    </div>
+  <header className="w-100">
+    <nav className="navbar d-flex justify-c-sb w-100">
+      <div className="nav d-flex justify-c-sb align-i-c">
+        <h1 className="brand">Bookstore CMS</h1>
+        <ul className="nav-list d-flex justify-c-sb">
+          <li className="nav-item">BOOKS</li>
+          <li className="nav-item">CATEGORIES</li>
+        </ul>
+      </div>
+      <div className="avatar-cont">
+        <img className="avatar" src={avatar} alt="avatar" />
+      </div>
+    </nav>
   </header>
 );
 
