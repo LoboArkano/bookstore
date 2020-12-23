2f5c4a3d59fccb4729787a1be4d9be329d6a6702
LoboArkano <pipiolo_mananes@hotmail.com>
2020-12-23 15:54:30 -0600

    Add style

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Header from './Header';
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
+import '../assets/stylesheets/app.css';
 
 const App = () => (
   <div>
     <Header />
-    <BooksList />
-    <BooksForm />
+    <div className="main w-100">
+      <BooksList />
+      <BooksForm />
+    </div>
   </div>
 );
 
